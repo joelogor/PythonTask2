cfa6a1bea59014f05123ee5d971dd8e0036f704e
joelogor <ogorjoelchukwuebuka@gmail.com>
2026-05-09 13:13:05 +0100

    Spicy-Snack

--- Spicy-Snack/calculator.py
+++ Spicy-Snack/calculator.py
@@ -1,9 +1,32 @@
-Get first_number from user
-Get operator from user (+, -, *, /, etc.)
-Get second_number from user
+# Get first number from user
+#Get operator from user (+, -, *, /, etc.)
+#Get second number from user
+#
+#Perform the arithmetic operation based on the given operator
+#Return the computed result
+#
+#Call the function
+#Print the final result
+#
 
-Perform the arithmetic operation based on the given operator
-Return the computed result
+first_number = int(input("Enter first number: "))
+operator = input("Enter operator: ")
+second_number = int(input("Enter second number: "))
 
-Call the function
-Print the final result
+
+def calculate(first_number, operator, second_number):
+    match operator:
+        case "+":
+            result = first_number + second_number
+        case "-":
+            result = first_number - second_number
+        case "*":
+            result = first_number * second_number
+        case "/":
+            result = first_number / second_number
+
+    return result
+    
+result =  calculate(first_number, operator, second_number)
+ 
+print(result)
